225ca0bea3d8dba18fef9d6341ae99022f432524
tadernielsen <tadernielsen@gmail.com>
2025-02-10 12:18:45 -0700

    Created view components

---
+++ src/devlog/devlog.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Devlog() {
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>devlog displayed here</div>
+    </main>
+  );
+}
