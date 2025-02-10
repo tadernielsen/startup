d8510777c66e3f76413f4300010630b295b41fb8
tadernielsen <tadernielsen@gmail.com>
2025-02-10 12:53:25 -0700

    Created Home and Games structure

--- src/games/games.jsx
+++ src/games/games.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
+import './games.css';
 
 export function Games() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>games displayed here</div>
+    <main>
+        <h2>Games</h2>
+        <a className="devButton" hidden><button>Add Game</button></a>
+        
+        <section className="games">
+          <div className="game">
+            <div className="gameInfo">
+              <img src="Images/placeholder.png"/>
+              <h3>Game 1</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+            </div>
+            <div className="gameButtons">
+              <a href=""><button className="like">👍</button></a>
+              <a href=""><button className="favorite">⭐</button></a>
+              <a href=""><button className="download">Download</button></a>
+            </div>
+            <a className="devButton" hidden><button>Edit</button></a>
+          </div>
+          <div className="game">
+            <div className="gameInfo">
+              <img src="Images/placeholder.png"/>
+              <h3>Game 2</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+            </div>
+            <div className="gameButtons">
+              <a href=""><button className="like">👍</button></a>
+              <a href=""><button className="favorite">⭐</button></a>
+              <a href=""><button className="download">Download</button></a>
+            </div>
+            <a className="devButton" hidden><button>Edit</button></a>
+          </div>
+          <div className="game">
+            <div className="gameInfo">
+              <img src="Images/placeholder.png"/>
+              <h3>Game 3</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam.</p>
+            </div>
+            <div className="gameButtons">
+              <a href=""><button className="like">👍</button></a>
+              <a href=""><button className="favorite">⭐</button></a>
+              <a href=""><button className="download">Download</button></a>
+            </div>
+            <a className="devButton" hidden><button>Edit</button></a>
+          </div>
+      </section>
     </main>
   );
 }
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import './home.css';
 
 export function Home() {
   return (
-    <main className='container-fluid bg-secondary text-center'>
-      <div>home displayed here</div>
+    <main>
+        <h2>Home</h2>
+        
+        <section className="latestAnnouncement">
+            <h3>Latest Announcement</h3>
+            <p className="latestAnouncement">No current announcements</p>
+            <a className="devButton" hidden><button>Edit</button></a>
+        </section>
+        
+        <section className="welcome">
+            <h3>Welcome</h3>
+            <p>Welcome to TN-Games! This website is designed for you to view and download my games in the <b>Games</b> tab. You can also view devlog posts in the <b>DevLog</b> Page. If you <b>Login</b>, you will be able to like devlog posts and favorite games</p>
+        </section>
+        
+        <section className="about">
+            <h3>About</h3>
+            <p>This website was created for my CS260 class. It is currently still in development. Development started <b>January 9th, 2025</b></p>
+        </section>
     </main>
   );
 }
