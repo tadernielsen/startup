cb7dddf3768ce74d70a84f56ae0b049aa970586e
tadernielsen <tadernielsen@gmail.com>
2025-02-13 17:36:25 -0700

    Annoucement React Added

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -2,14 +2,40 @@ import React from 'react';
 import './home.css';
 
 export function Home() {
+  const [announcement, setAnnouncement] = React.useState('No current announcements');
+  const [editing, setEditing] = React.useState(false);
+
+  const editClick = () => {
+    setEditing(true);
+  }
+
+  const saveClick = () => {
+    setAnnouncement(document.getElementById('announcement').value);
+    localStorage.setItem('announcement', document.getElementById('announcement').value);
+    setEditing(false);
+  }
+
+  React.useEffect(() => {
+    setAnnouncement(localStorage.getItem('announcement')) || 'No current announcements';
+  }, []);
+
   return (
     <main className="home">
         <h2>Home</h2>
         
         <section className="latestAnnouncement">
             <h3>Latest Announcement</h3>
-            <p className="latestAnouncement">No current announcements</p>
-            <button className="devButton">Edit</button>
+            {editing ? (
+              <div>
+                <div><textarea type="textarea" id="announcement" placeholder="Enter announcement" style={{width: '100%', height: 'auto'}}></textarea></div>
+                <button className="devButton" onClick={saveClick}>Save</button>
+              </div>
+            ) : (
+              <div>
+                <p>{announcement}</p>
+                <button className="devButton" onClick={editClick}>Edit</button>
+              </div>
+            )}
         </section>
         
         <section className="welcome">
