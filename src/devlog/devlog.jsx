e5904f52addb8856f23b00fc14570246b80cda02
tadernielsen <tadernielsen@gmail.com>
2025-02-20 20:09:14 -0700

    Created Log Class

--- src/devlog/devlog.jsx
+++ src/devlog/devlog.jsx
@@ -1,9 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './devlog.css';
+
+class DevlogPost
+{
+  #liked = false;
+
+  constructor(title, description, likeCount = 0)
+  {
+    this.title = title
+    this.description = description
+    this.likeCount = likeCount
+  }
+
+  likeButton()
+  {
+    if (this.#liked)
+    {
+      this.likeCount -= 1;
+      this.#liked = false;
+    }
+    else
+    {
+      this.likeCount += 1;
+      this.#liked = true;
+    }
+  }
+
+  initilizePost()
+  {
+    return (
+      <div className="log">
+            <h3>{this.title}</h3>
+            <p>{this.description}</p>
+            <div className="likeCounter">
+              <p><b>{this.likeCount}</b></p>
+              <button className="like" onClick={this.likeButton}>👍</button>
+            </div>
+            <button className="devButton">Edit</button>
+      </div>
+    )
+  }
+
+  returnJson()
+  {
+    return {"title": this.title, "description": this.description, "likeCount": this.likeCount}
+  }
+}
 
 export function Devlog() {
   const [logs, setLogs] = React.useState([]);
   const [newPost, setNewPost] = React.useState(false);
+
+  useEffect(() => {
+    const savedLogs = localStorage.getItem('devLogs')
+    if (savedLogs)
+    {
+      setLogs()// Need to have it set the savedLogs to the logs var
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('devLogs', JSON.stringify(logs));
+  }, [logs]);
 
   function addNewPost()
   {
@@ -19,14 +77,9 @@ export function Devlog() {
 
   function savePost(title, description)
   {
-    const newPost = {"logTitle": title, 'logDescription': description, "logLikes": 0};
-    logs.push(newPost);
-    localStorage.setItem('devLogs', logs);
-  }
+    const newPost = new DevlogPost(title, description);
 
-  function likePost()
-  {
-    
+    setNewPost(false);
   }
 
   return (
@@ -40,7 +93,7 @@ export function Devlog() {
             <div className="log newlog">
               <input type="text" id="logTitle" placeholder='Title'></input>
               <textarea type="textarea" id="logDescription" placeholder="Enter description here"></textarea>
-              <button className="saveButton" onClick={() => savePost("hellow", "world")}>Post</button>
+              <button className="saveButton" onClick={() => savePost(document.getElementById('logTitle').value, document.getElementById('logDescription').value)}>Post</button>
             </div>
             ) : (null)
           }
