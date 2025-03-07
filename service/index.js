d75c6f9915401e87c5b0e44203d7b859627e9881
tadernielsen <tadernielsen@gmail.com>
2025-03-06 18:55:51 -0700

    User Creation Endpoint Created

--- service/index.js
+++ service/index.js
@@ -4,9 +4,10 @@ const express = require('express');
 const app = express();
 
 // Login stuff
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const cookieParser = require('cookie-parser');
 const uuid = require('uuid');
+var apiRouter = express.Router();
 
 const authCookieName = 'token';
 
@@ -19,5 +20,64 @@ const games = [];
 
 const announcement = 'Press Edit to change this!';
 
+// Functions
+async function createUser(username, password, userType)
+{
+    const hashedPassword = await bcrypt.hash(password, 10);
 
+    const user = {name: username, pass: hashedPassword, type: userType};
+    users.push(user);
+
+    return user;
+}
+
+async function getUser(field, value)
+{
+    if (value)
+    {
+        return users.find(user => user[field] === value);
+    }
+    return null;
+}
+
+function setCookie(res, user)
+{
+    user.token = uuid.v4();
+    res.cookie(
+        'token', user.token, 
+        {
+        secure: true, 
+        httpOnly: true, 
+        sameSite: 'strict',
+    });
+}
+
+// Middleware
 app.use(express.static('public'));
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+app.use('/api', apiRouter);
+
+// Endpoints
+// User Creation
+app.post('/auth/CreateAccount', async (req, res) => {
+    if (await getUser('email', req.body.email))
+    {
+        res.status(409).send({msg: 'User already exists.'});
+    }
+    else
+    {
+        const user = await createUser(req.body.email, req.body.password, req.body.type);
+        
+        setCookie(res, user);
+
+        res.send({email: user.name, type: user.type});
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
