079e2c6aac3546ce8657606aee1ba0576d08982a
tadernielsen <tadernielsen@gmail.com>
2025-03-06 17:23:12 -0700

    Added login service variables

--- service/index.js
+++ service/index.js
@@ -1,3 +1,13 @@
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+// Express stuff
+const express = require('express');
+const app = express();
+
+// Login stuff
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const uuid = require('uuid');
+
+const authCookieName = 'token';
 
 app.use(express.static('public'));
