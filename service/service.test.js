cd5ddcfe142a7816c47da105fb0d95f45da35229
tadernielsen <tadernielsen@gmail.com>
2025-03-19 19:59:37 -0600

    Added More Tests

--- service/service.test.js
+++ service/service.test.js
@@ -1,14 +1,55 @@
 const request = require('supertest');
 const app = require('./service')
 
-test('Register Simple', async () => {
-    const email = "test1@email.com";
-    const password = 'secret';
-    const registerUser = await request(app).post('/api/auth/CreateAccount').send({email, password});
+// User Tests
+function getRandomName()
+{
+    const min = 1;
+    const max = 500;
+    return `email${Math.floor(Math.random() * (max - min + 1) + min)}@tader.TN`;
+}
+
+function createUser()
+{
+    const email = getRandomName();
+    const password = "secret :)";
+    return {email: email, password: password}
+}
+
+test('Register', async () => {
+    const user = await createUser();
+    const registerUser = await request(app).post('/api/auth/CreateAccount').send(user);
 
     expect(registerUser.status).toBe(200);
     expect(registerUser.headers['content-type']).toMatch('application/json; charset=utf-8');
-    expect(registerUser.body).toMatchObject({email, type:'normal'});
+    expect(registerUser.body).toMatchObject({email:user.email, type:'normal'});
+});
+
+test('Login', async () => {
+
+});
+
+test('Logout', async () => {
+
+});
+
+test('Get User', async () => {
+
+});
+
+// Developer Test
+test('Developer Login', async () => {
+
+});
+
+// Announcement Test
+// Add function that creates random announcement and saves it
+test('Save Announcement', async () => {
+
+});
+
+test('Get Announcement', async () => {
+
 });
 
 afterAll(() => {
