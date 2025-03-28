4107eb247eba2d2a8235f0b6bfa31dfd7cb5b793
tadernielsen <tadernielsen@gmail.com>
2025-03-27 19:53:33 -0600

    Added WebSocket Backend

---
+++ service/serverProxy.js
@@ -0,0 +1,35 @@
+const { WebSocketServer } = require('ws');
+
+function serverProxy(server)
+{
+    const socketServer = new WebSocketServer({server});
+
+    socketServer.on('connection', (socket) => {
+        socket.isAlive = true;
+
+        socket.on('message', function message(data) {
+            socketServer.clients.forEach((client) => {
+                if (client !== socket && client.readyState === WebSocketServer.OPEN) {
+                    client.send(data);
+                }
+            });
+        });
+
+        socket.on('pong', () => {
+            socket.isAlive = true;
+        });
+    });
+
+    setInterval(() => {
+        socketServer.clients.forEach((client) => {
+            if (client.isAlive === false) {
+                return client.terminate();
+            }
+
+            client.isAlive = false;
+            client.ping();
+        });
+    }, 30000);
+}
+
+module.exports = serverProxy;
